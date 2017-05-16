35dace1ddaa44d0e601609c3fb71b3c6b633944c
Sage Lichtenwalner <sage@marine.rutgers.edu>
2017-05-16 17:05:00 -0400

    Style tweak to Productivity Instructor's Guide

--- productivity/guide.php
+++ productivity/guide.php
@@ -26,14 +26,25 @@
 <h3>Teaching Notes</h3>
 <p>The Data Explorations require access to an internet-ready computer or tablet. </p><p>Note, the Data Explorations use authentic raw data. There is no downsampling of any data, thus the amount of data over time varies within the interactive based on the original data and similarly some data is missing.</p><p>There are 7 different activities to choose from within this collection:</p>
 
-<h4><a href="activity0.php">Primary Production in General</a></h4>
+
+<div class="panel panel-primary">
+  <div class="panel-heading">
+    <h3 class="panel-title"><a href="activity0.php">Primary Production in General</a></h3>
+  </div>
+  <div class="panel-body">
 <p>Through using an online concept-mapping tool students can:</p>
 <ul>
   <li>Access their prior knowledge of the material before the unit (Invitation).</li>
   <li>Demonstrate their gained knowledge after the unit (Reflection).</li>
 </ul>
+  </div>
+</div>
 
-<h4><a href="activity1.php">Primary Production Variables</a></h4>
+<div class="panel panel-primary">
+  <div class="panel-heading">
+    <h3 class="panel-title"><a href="activity1.php">Primary Production Variables</a></h3>
+  </div>
+  <div class="panel-body">
 <p>Students are able to explore Chlorophyll-a Concentrations, Colored Dissolved Organic Matter, Optical Backscatter, and Total Scattering Coefficient of Pure Seawater as different variables that are collected from the 3-wavelength fluorometer to investigate primary production. The data are from the Northern Pacific Ocean (<a href="http://oceanobservatories.org/array/coastal-endurance/">Coastal Endurance Array</a>) from April 5, 2015 to August 7, 2016. The students can:</p>
 <ul>
   <li>Explore what they can observe in different kinds of data that are collected in regards to primary production (Exploration).</li>
@@ -41,24 +52,42 @@
 </li>
   <li>Investigate the relationship between Chlorophyll-a Concentrations and Colored Dissolved Organic Matter and how the connects to their prior understanding of primary production (Application).</li>
 </ul>
+  </div>
+</div>
 
-<h4><a href="activity2.php">Chlorophyll Across a Year</a></h4>
+<div class="panel panel-primary">
+  <div class="panel-heading">
+    <h3 class="panel-title"><a href="activity2.php">Chlorophyll Across a Year</a></h3>
+  </div>
+  <div class="panel-body">
 <p>Students explore Chlorophyll-a Concentrations collected from the 3-wavelength fluorometer to investigate patterns in primary production over a year. The data are from the Temperate Pacific Ocean (<a href="http://oceanobservatories.org/array/coastal-endurance/">Coastal Endurance Array</a>) from May 31, 2015 to May 31, 2016. The students can:</p>
 <ul>
   <li>Explore what they can observe in Chlorophyll data over a year (Exploration).</li>
   <li>Determine what kinds of seasonal patterns they can observe in primary production (Concept Invention).</li>
   <li>Investigate the how seasonal primary production varies over the year and how the connects to their prior understanding of primary production (Application).</li>
 </ul>
+  </div>
+</div>
 
-<h4><a href="activity3.php">Chlorophyll Across the Globe</a></h4>
+<div class="panel panel-primary">
+  <div class="panel-heading">
+    <h3 class="panel-title"><a href="activity3.php">Chlorophyll Across the Globe</a></h3>
+  </div>
+  <div class="panel-body">
 <p>Students explore Chlorophyll-a Concentrations collected from the 3-wavelength fluorometer to investigate patterns in primary production from different areas around the Earth during the Fall. The data are from six OOI stations: northern near polar Pacific Ocean (<a href="http://oceanobservatories.org/array/global-station-papa/">Station Papa Array</a>); northern temperate Pacific Ocean (<a href="http://oceanobservatories.org/array/coastal-endurance/">Coastal Endurance Array</a>); southern near polar Pacific Ocean (<a href="http://oceanobservatories.org/array/global-southern-ocean/">Southern Ocean Array</a>); northern near polar Atlantic Ocean (<a href="http://oceanobservatories.org/array/global-irminger-sea/">Irminger Sea Array</a>); northern temperate Atlantic Ocean (<a href="http://oceanobservatories.org/array/coastal-pioneer/">Coastal Pioneer Array</a>); southern near polar Atlantic Ocean (<a href="http://oceanobservatories.org/array/global-argentine-basin/">Argentine Basin Array</a>). The students can:</p>
 <ul>
   <li>Explore what they can observe in Chlorophyll data in different locations (Exploration).</li>
   <li>Determine what kinds of global patterns they can observe in primary production (Concept Invention).</li>
   <li>Investigate the how primary production varies around the Earth in the Fall and how the connects to their prior understanding of primary production (Application).</li>
 </ul>
+  </div>
+</div>
 
-<h4><a href="activity4.php">Chlorophyll-a in Temperate Zones of the Ocean</a></h4>
+<div class="panel panel-primary">
+  <div class="panel-heading">
+    <h3 class="panel-title"><a href="activity4.php">Chlorophyll-a in Temperate Zones of the Ocean</a></h3>
+  </div>
+  <div class="panel-body">
 <p>Students explore Chlorophyll-a Concentrations collected from the 3-wavelength fluorometer to investigate patterns in primary production from different sites in the Temperate Zones of the Ocean. The data are from OOI sites in temperate latitudes from the Pacific Ocean (<a href="http://oceanobservatories.org/array/coastal-endurance/">Coastal Endurance Array</a>) and Atlantic Ocean (<a href="http://oceanobservatories.org/array/coastal-pioneer/">Coastal Pioneer Array</a>; <a href="http://oceanobservatories.org/array/global-argentine-basin/">Argentine Basin Array</a>). The data are from March 17, 2015 to February 29, 2016, though not all sites have data from this full time span. The students can:</p>
 <ul>
   <li>Explore what they can observe in Chlorophyll data at temperate latitudes (Exploration).</li>
@@ -66,22 +95,36 @@
   <li>Investigate similarities and differences in observed patterns among the temperate latitude locations and how the connects to their prior understanding of primary production (Application).
 </li>
 </ul>
+  </div>
+</div>
 
-<h4><a href="activity5.php">Chlorophyll-a Near Polar Zones of the Ocean</a></h4>
+<div class="panel panel-primary">
+  <div class="panel-heading">
+    <h3 class="panel-title"><a href="activity5.php">Chlorophyll-a Near Polar Zones of the Ocean</a></h3>
+  </div>
+  <div class="panel-body">
 <p>Students explore Chlorophyll-a Concentrations collected from the 3-wavelength fluorometer to investigate patterns in primary production from different sites near the Polar Zones of the Ocean. The data are from OOI sites near or just at polar latitudes from the Pacific Ocean (<a href="http://oceanobservatories.org/array/global-station-papa/">Station Papa Array</a>; <a href="http://oceanobservatories.org/array/global-southern-ocean/">Southern Ocean Array</a>) and Atlantic Ocean (<a href="http://oceanobservatories.org/array/global-irminger-sea/">Irminger Sea Array</a>). The data are from February 25, 2015 to January 1, 2016, though not all sites have data from this full time span. The students can:</p>
 <ul>
   <li>Explore what they can observe in Chlorophyll data near or just at polar latitudes (Exploration).</li>
   <li>Determine what kinds of patterns they can observe in primary production over time near or just at polar latitudes (Concept Invention).</li>
   <li>Investigate similarities and differences in observed patterns among the near or just at polar latitude locations and how the connects to their prior understanding of primary production (Application).</li>
 </ul>
+  </div>
+</div>
 
-<h4><a href="activity6.php">Chlorophyll-a Inshore vs. Offshore</a></h4>
+<div class="panel panel-primary">
+  <div class="panel-heading">
+    <h3 class="panel-title"><a href="activity6.php">Chlorophyll-a Inshore vs. Offshore</a></h3>
+  </div>
+  <div class="panel-body">
 <p>Students explore Chlorophyll-a Concentrations collected from the 3-wavelength fluorometer to investigate patterns in primary production among inshore (~6 km from shore, in 29m of water OR 95km from shore, in 92m of water) and offshore (~29km from shore, in 87m of water OR 177km from shore, in 133m of water) locations. Note, due to the differences in geology and ocean currents in the Pacific and Atlantic Oceans it may be important to talk with your students about the differences in location from shore and at water depths. The data are from OOI sites in temperate latitudes from the Pacific Ocean (<a href="http://oceanobservatories.org/array/coastal-endurance/">Coastal Endurance Array</a>) and Atlantic Ocean (<a href="http://oceanobservatories.org/array/coastal-pioneer/">Coastal Pioneer Array</a>). The data are from April 30, 2015 to May 30, 2016, though not all sites have data from this full time span. The students can:</p>
 <ul>
   <li>Explore what they can observe in Chlorophyll data near or just at polar latitudes (Exploration).</li>
   <li>Determine what kinds of patterns they can observe in primary production over time near or just at polar latitudes (Concept Invention).</li>
   <li>Investigate similarities and differences in observed patterns among the near or just at polar latitude locations and how the connects to their prior understanding of primary production (Application).</li>
 </ul>
+  </div>
+</div>
 
 <h3>Subject / Topics</h3>
 <p>Introduction to Oceanography / Primary Production</p>
