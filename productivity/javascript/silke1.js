e3bf2206e37fe910d5b2621c06d4bc040cc6af92
Sage Lichtenwalner <sage@marine.rutgers.edu>
2018-04-04 09:23:21 -0400

    Added quick activity for Silke

---
+++ productivity/javascript/silke1.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+
+g = new Dygraph(document.getElementById("chart"), "data/silke1.csv", {
+  //title: 'Fluorometric Chlorophyll A Concentration',
+  // xlabel
+  ylabel: 'Chlorophyll-a Concentration (&micro;g/L)',
+  y2label: 'Nitrate Concentration (&micro;Mol/L)',
+  //labels : ["Date","Endurance","Pioneer"],
+  legend: 'always',
+  labelsDivStyles: { 'textAlign': 'right' },
+  labelsDivWidth : 600,
+  labelsUTC : true,
+  colors : ["#00457C","#DBA53A","#008100","#00839C","#00C6B0"],
+  strokeWidth: 0,
+  drawPoints: true,
+  pointSize: 2,
+  highlightCircleSize: 6,
+  //logscale : true,
+  showRangeSelector: true,
+  //rangeSelectorPlotFillColor : "#00839C",
+  //rangeSelectorPlotStrokeColor : "#008100",
+  //animatedZooms : true,
+  axes: {
+    y: {valueRange: [0, null]},
+    y2: {axisLabelWidth: 70}
+  },
+  series: {
+    'Washington Chl-a': {axis: 'y', showInRangeSelector: true},
+    'Washington Nitrate': {axis: 'y2'},
+    'Irminger Chl-a': {axis: 'y'},
+    'Irminger Nitrate': {axis: 'y2'},
+  },
+  visibility: [1,0,1,0],
+});
+
+}); //document.ready
+
+function toggle_visibility(el) {
+  g.setVisibility(parseInt(el.id), el.checked);
+}
