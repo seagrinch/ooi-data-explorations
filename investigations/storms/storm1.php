dc682991b116d9ae4d3a5deccefd787800022fc7
Sage Lichtenwalner <sage@marine.rutgers.edu>
2019-03-25 14:28:57 -0400

    Reorder stations in Storms Investigation

--- investigations/storms/storm1.php
+++ investigations/storms/storm1.php
@@ -9,16 +9,18 @@
 
 <div id="chart1" style="width:800px; height: 400px;"></div>
 <br>
+<p><strong>Select a parameter:</strong>
 <select id="select1" onchange="select_graph(this)">
-  <option value="0">44009 Air Pressure</option>
-  <option value="1">44009 Wave Height</option>
-  <option value="2">44009 Ocean Temperature</option>
-  <option value="3">44009 Wind Speed</option>
-  <option value="4">44065 Air Pressure</option>
-  <option value="5">44065 Wave Height</option>
-  <option value="6">44065 Ocean Temperature</option>
-  <option value="7">44065 Wind Speed</option>
+  <option value="4">Offshore NJ (44065) Air Pressure</option>
+  <option value="5">Offshore NJ (44065) Wave Height</option>
+  <option value="6">Offshore NJ (44065) Ocean Temperature</option>
+  <option value="7">Offshore NJ (44065) Wind Speed</option>
+  <option value="0" selected>Delaware (44009) Air Pressure</option>
+  <option value="1">Delaware (44009) Wave Height</option>
+  <option value="2">Delaware (44009) Ocean Temperature</option>
+  <option value="3">Delaware (44009) Wind Speed</option>
 </select>
+</p>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="../../js/dygraph-combined-dev.js"></script>
--- investigations/storms/storm2.php
+++ investigations/storms/storm2.php
@@ -9,25 +9,26 @@
 
 <div id="chart1" style="width:800px; height: 400px;"></div>
 <br>
-<strong>Select a parameter:</strong>
+<p><strong>Select a parameter:</strong>
 <select id="select1" onchange="select_graph(this)">
-  <option value="0">44009 Air Pressure</option>
-  <option value="1">44009 Wave Height</option>
-  <option value="2">44009 Ocean Temperature</option>
-  <option value="3">44009 Wind Speed</option>
-  <option value="4">44025 Air Pressure</option>
-  <option value="5">44025 Wave Height</option>
-  <option value="6">44025 Ocean Temperature</option>
-  <option value="7">44025 Wind Speed</option>
-  <option value="8">44027 Air Pressure</option>
-  <option value="9">44027 Wave Height</option>
-  <option value="10">44027 Ocean Temperature</option>
-  <option value="11">44027 Wind Speed</option>
-  <option value="12">44065 Air Pressure</option>
-  <option value="13">44065 Wave Height</option>
-  <option value="14">44065 Ocean Temperature</option>
-  <option value="15">44065 Wind Speed</option>
+  <option value="8">Maine (44027) Air Pressure</option>
+  <option value="9">Maine (44027) Wave Height</option>
+  <option value="10">Maine (44027) Ocean Temperature</option>
+  <option value="11">Maine (44027) Wind Speed</option>
+  <option value="4">NY Harbor (44025) Air Pressure</option>
+  <option value="5">NY Harbor (44025) Wave Height</option>
+  <option value="6">NY Harbor (44025) Ocean Temperature</option>
+  <option value="7">NY Harbor (44025) Wind Speed</option>
+  <option value="12">Offshore NJ (44065) Air Pressure</option>
+  <option value="13">Offshore NJ (44065) Wave Height</option>
+  <option value="14">Offshore NJ (44065) Ocean Temperature</option>
+  <option value="15">Offshore NJ (44065) Wind Speed</option>
+  <option value="0" selected>Delaware (44009) Air Pressure</option>
+  <option value="1">Delaware (44009) Wave Height</option>
+  <option value="2">Delaware (44009) Ocean Temperature</option>
+  <option value="3">Delaware (44009) Wind Speed</option>
 </select>
+</p>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="../../js/dygraph-combined-dev.js"></script>
