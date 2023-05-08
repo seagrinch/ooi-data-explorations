b7458f9c0f249a7b4041a24f179d6e961057a09a
Sage Lichtenwalner <sage@marine.rutgers.edu>
2023-05-08 11:26:39 -0400

    Corrected Google Analytics tag

--- footer.php
+++ footer.php
@@ -31,14 +31,14 @@
     ?>
 
     <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-CWLQX0DXX8"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-RZXFGFC587"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
     
-      gtag('config', 'G-CWLQX0DXX8');
+      gtag('config', 'G-RZXFGFC587');
     </script>
-
+    
   </body>
 </html>
